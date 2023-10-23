9240d091229800bf5998fd0e0923f8532a596f70
MiaTian1024 <sddxtm123@gmail.com>
2023-10-24 11:39:10 +1300

    update admin page

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -21,3 +21,25 @@ search.addEventListener("keyup", () =>{
         image.style.display = "flex";
     })
 })
+
+
+// Open the modal
+const openModalButton = document.getElementById('openModalButton');
+const modal = document.getElementById('myModal');
+const closeModalButton = document.getElementById('closeModalButton');
+
+openModalButton.addEventListener('click', function() {
+    modal.style.display = 'block';
+});
+
+// Close the modal
+closeModalButton.addEventListener('click', function() {
+    modal.style.display = 'none';
+});
+
+// Click outside the modal to close it
+window.addEventListener('click', function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+});
