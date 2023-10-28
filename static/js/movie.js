2f2487520f5210c77da5d45fe94c7d7353f7c5be
MiaTian1024 <sddxtm123@gmail.com>
2023-10-28 20:01:16 +1300

    screening style

--- static/js/movie.js
+++ static/js/movie.js
@@ -1,3 +1,16 @@
+// Get a reference to the form element
+const form = document.getElementById('filter-form');
+
+// Get a reference to the radio buttons
+const radioButtons = form.querySelectorAll('input[type="radio"]');
+
+// Add an event listener to each radio button
+radioButtons.forEach((radioButton) => {
+    radioButton.addEventListener('change', function () {
+        // Automatically submit the form when a radio button is selected
+        form.submit();
+    });
+});
 
 // Open the modal
 const openModalButton = document.getElementById('openModalButton');
